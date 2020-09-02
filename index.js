562d6797360ee1bb46c26fdd91904c41d08d000b
ko <ko>
2020-09-02 23:07:37 +0900

    Express Core : Middlewares

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // requre -> node module을 어딘가에서 가져오는것 express라는 폴더를 현재 가지고있는 폴더나 폴더에 없으면 node module안에 express라는 폴더를 찾는다.
 // const express = require('express');
 import express from "express";
+import morgan from "morgan";
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 //그리고 app에 express를 실행해서 담는다.
 const app = express()
 
 const PORT = 4000;
+
+
 
 const handleListening = () => {
     console.log(`listening on: http://localhost:${PORT}`);
@@ -20,9 +26,15 @@ const hadnlePropfile = (req, res) => {
     res.send("your on my profile")
 }
 
+//helmet사용은 보안을 위해서 사용한다.
+app.use(cookieParser());
+app.use(bodyParser.json({extends : true}));
+app.use(bodyParser.urlencoded({extends : true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 //라우트 형식
-app.get("/", handleHome);
+app.get("/" ,handleHome);
 
 app.get("/profile", hadnlePropfile);
 
