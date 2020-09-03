5cbb431734ce79bfd2a7baa89861e246a70eccaf
ko <ko>
2020-09-03 17:33:39 +0900

    Join, Login, Change Profile HTML

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # KTube
 
 cloning youtube
+
+
+## Page :
+    
+    - [ ] Home
+    - [x] Join
+    - [ ] Login
+    - [x] Search
+    - [ ] User Detail
+    - [ ] Edit Profile
+    - [ ] Change Password
+    - [ ] Upload
+    - [ ] Video Detail
+    - [ ] Edit Video
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 export const join = (req, res) => res.render("join", {pageTitle : 'Join'});
-export const login = (req, res) => res.render("Login", {pageTitle : 'Log In'});
-export const logout = (req, res) => res.render("Logout", {pageTitle : 'Log Out'});
-export const userDetail = (req, res) => res.render("User Detail", {pageTitle : 'User Detail'});
-export const editProfile = (req, res) => res.render("Edit Profile", {pageTitle : 'Edit Profile'});
-export const changePassword = (req, res) => res.render("Change Password", {pageTitle : 'Change Password'});
+export const login = (req, res) => res.render("login", {pageTitle : 'Log In'});
+export const logout = (req, res) => res.render("logout", {pageTitle : 'Log Out'});
+export const userDetail = (req, res) => res.render("userDetail", {pageTitle : 'User Detail'});
+export const editProfile = (req, res) => res.render("editProfile", {pageTitle : 'Edit Profile'});
+export const changePassword = (req, res) => res.render("changePassword", {pageTitle : 'Change Password'});
